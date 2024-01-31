c48300fa7773e18a6d70afb3b101b87b58576970
Marc Gallardo <marc.gallardo.contractor@bbva.com>
2024-01-31 09:49:56 +0100

    init commit

---
+++ PokedexKanto/PokedexKanto/PokedexKantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexKantoApp.swift
+//  PokedexKanto
+//
+//  Created by Marc Gallardo Ruiz on 31/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexKantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
